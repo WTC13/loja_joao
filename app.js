11c3ed393ba58b8c300afe542ba86e9761b6c875
Wendell Costa <wendell.timoteoc@gmail.com>
2026-04-17 01:59:43 -0300

    create project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
+const port = 4725;
+
+// view engine
+app.engine("handlebars", engine({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
+// routes 
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.listen(port, function(req, res){
+    console.log(`server running in: http://localhost:${port}"`);
+});
