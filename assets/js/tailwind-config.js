9baa28f64f0520c1e4cdb023cc207b3101f1ebbb
Wendell Costa <wendell.timoteoc@gmail.com>
2026-04-17 02:19:11 -0300

    update project

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme:{
+        extend:{
+            colors:{
+                brand: "#001010",
+                layout: {
+                    night: "#0a0a0a",   // Fundo principal (quase preto)
+                    card: "#121212",    // Para cards de produtos se destacarem do fundo
+                    border: "#262626",  // Linhas divisórias sutis
+                    zinc: "#18181b",    // Um tom mais puxado para o metal
+                },
+            }
+        }
+    }
+}
